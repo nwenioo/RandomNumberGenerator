26ebbcb9234d2c508bda57723f7387070e4af202
nwenioo <nwenioo@example.com>
2024-02-18 14:56:41 +0630

    Add project files.

---
+++ RandomNumberGenerator/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RandomNumberGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberLable = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numberLable
+            // 
+            this.numberLable.Font = new System.Drawing.Font("Trebuchet MS", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberLable.Location = new System.Drawing.Point(74, 36);
+            this.numberLable.Name = "numberLable";
+            this.numberLable.Size = new System.Drawing.Size(312, 87);
+            this.numberLable.TabIndex = 0;
+            this.numberLable.Text = "Welcome :)";
+            this.numberLable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateButton.Location = new System.Drawing.Point(137, 189);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(168, 56);
+            this.generateButton.TabIndex = 1;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 282);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.numberLable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = " Random Number Generator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numberLable;
+        private System.Windows.Forms.Button generateButton;
+    }
+}
+
---
+++ RandomNumberGenerator/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RandomNumberGenerator;
+
+namespace RandomNumberGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r=new Random();
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            //int randomNumber=r.Next();
+            //upper boundary to randomize 
+            //int randomNumber = r.Next(10);
+            //upper boundary and lower boundary -number within this range 
+            int randomNumber = r.Next(1,100);
+            numberLable.Text= randomNumber.ToString();
+        }
+    }
+}
